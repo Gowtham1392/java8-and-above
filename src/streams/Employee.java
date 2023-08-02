c5a854060d45e0b561a963c5ddb601d6186ebd53
gowtham <gowtham.1392@gmail.com>
2023-08-02 20:46:51 +0900

    Temporarily saved Employee class

---
+++ src/streams/Employee.java
@@ -0,0 +1,20 @@
+package streams;
+
+public class Employee {
+	
+	private int id;
+	private String first_name;
+	private String last_name;
+	private long salary;
+	private String dept;
+	public Employee(int id, String first_name, String last_name, long salary, String dept) {
+		super();
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.salary = salary;
+		this.dept = dept;
+	}
+	
+	
+}
